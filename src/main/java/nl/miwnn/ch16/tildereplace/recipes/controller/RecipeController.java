6572b3108b273c38bc7038b5a6b968496817dc94
Remi <heyremi@gmail.com>
2025-06-25 21:32:36 +0200

    Updated mapping to show recipeDetails

--- src/main/java/nl/miwnn/ch16/tildereplace/recipes/controller/RecipeController.java
+++ src/main/java/nl/miwnn/ch16/tildereplace/recipes/controller/RecipeController.java
@@ -1,10 +1,16 @@
 package nl.miwnn.ch16.tildereplace.recipes.controller;
 
+import nl.miwnn.ch16.tildereplace.recipes.model.Ingredient;
+import nl.miwnn.ch16.tildereplace.recipes.model.Recipe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import nl.miwnn.ch16.tildereplace.recipes.repository.RecipeRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class RecipeController {
@@ -15,9 +21,28 @@ public class RecipeController {
         this.recipeRepository = recipeRepository;
     }
 
+    private String setupRecipeDetail(Model datamodel, Recipe recipeToShow) {
+        datamodel.addAttribute("recipe", recipeToShow);
+        List<Ingredient> allIngredients = recipeToShow.getIngredients();
+        datamodel.addAttribute("theIngredients", allIngredients);
+
+        return "recipeDetails";
+    }
+
     @GetMapping({"/", "/recipeOverview"})
     private String showRecipeOverview(Model datamodel) {
         datamodel.addAttribute("allRecipes", recipeRepository.findAll());
         return "recipeOverview";
+    }
+
+    @GetMapping("/recipe/detail/{recipeName}")
+    private String showTest(@PathVariable("recipeName") String name, Model datamodel) {
+
+        Optional<Recipe> recipeOptional = recipeRepository.findRecipeByRecipeName(name);
+
+        if (recipeOptional.isEmpty()) {
+            return "redirect:/recipeOverview";
+        }
+        return setupRecipeDetail(datamodel, recipeOptional.get());
     }
 }
