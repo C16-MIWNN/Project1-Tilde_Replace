87422f3061d17a3290109a05f6b0423dec7deb9e
Dennis Mei <dmei22@hotmail.com>
2025-06-26 14:04:28 +0200

    Added paths to FoodController

--- src/main/java/nl/miwnn/ch16/tildereplace/recipes/controller/FoodController.java
+++ src/main/java/nl/miwnn/ch16/tildereplace/recipes/controller/FoodController.java
@@ -1,11 +1,14 @@
 package nl.miwnn.ch16.tildereplace.recipes.controller;
 
+import nl.miwnn.ch16.tildereplace.recipes.model.Food;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import nl.miwnn.ch16.tildereplace.recipes.repository.FoodRepository;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/food")
@@ -22,4 +25,45 @@ public class FoodController {
         datamodel.addAttribute("allFoods", foodRepository.findAll());
         return "foodOverview";
     }
+
+    @GetMapping("/new")
+    private String newIngredient(Model dataModel) {
+        dataModel.addAttribute("foodForm", new Food());
+
+        return "foodForm";
+    }
+
+    @GetMapping("/edit/{foodId}")
+    private String editFood(@PathVariable("foodId") Long foodId, Model dataModel) {
+        Optional<Food> foodOptional = foodRepository.findById(foodId);
+        if (foodOptional.isPresent()) {
+            dataModel.addAttribute("foodForm", foodOptional.get());
+        }
+
+        return "foodForm";
+    }
+
+    @PostMapping("/save")
+    private String saveOrUpdateFood(@ModelAttribute("foodForm") Food toBeSavedFood,
+                                          BindingResult result) {
+        if (result.hasErrors()) {
+            System.err.println(result.getAllErrors());
+        } else {
+            foodRepository.save(toBeSavedFood);
+        }
+
+        return "redirect:/food/foodOverview";
+    }
+
+    @GetMapping("/delete/{foodId}")
+    private String deleteFood(@PathVariable("foodId") Long foodId) {
+        Optional<Food> foodOptional = foodRepository.findById(foodId);
+        if (foodOptional.isPresent()) {
+            foodRepository.deleteById(foodId);
+        }
+
+        return "redirect:/food/foodOverview";
+    }
+
+
 }
--- src/main/java/nl/miwnn/ch16/tildereplace/recipes/repository/FoodRepository.java
+++ src/main/java/nl/miwnn/ch16/tildereplace/recipes/repository/FoodRepository.java
@@ -5,6 +5,8 @@ import nl.miwnn.ch16.tildereplace.recipes.model.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FoodRepository extends JpaRepository<Food, Long> {
 }
