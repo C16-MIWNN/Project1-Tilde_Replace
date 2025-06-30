a0e9d039756a4ca65d2ca166b9b3cb1869b5bb97
Remi <heyremi@gmail.com>
2025-06-30 12:58:22 +0200

    Add array initialization for Default Recipe

--- src/main/java/nl/miwnn/ch16/tildereplace/recipes/dto/NewRecipeDTO.java
+++ src/main/java/nl/miwnn/ch16/tildereplace/recipes/dto/NewRecipeDTO.java
@@ -2,19 +2,38 @@ package nl.miwnn.ch16.tildereplace.recipes.dto;
 
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class NewRecipeDTO {
+    private static final int DEFAULT_NUMBER_RECIPE_FIELDS = 3;
 
+    private Long id;
     private String recipeName;
     private String preparationInstruction;
 
-    private ArrayList<Long> foodIds;
-    private ArrayList<Integer> ingredientQuantities;
-    private ArrayList<Long> unitIds;
+    private ArrayList<Long> foodIds = new ArrayList<>();
+    private ArrayList<Integer>  ingredientQuantities = new ArrayList<>();
+    private ArrayList<Long> unitIds = new ArrayList<>();
+
+    public NewRecipeDTO() {
+       InitializeDefaultRecipe();
+    }
+
+    public void InitializeDefaultRecipe(){
+
+        for (int index = 0; index < DEFAULT_NUMBER_RECIPE_FIELDS; index++) {
+            foodIds.add(index,getId());
+            ingredientQuantities.add(index,0);
+            unitIds.add(index,getId());
+        }
+
+    }
 
     public String getRecipeName() {
         return recipeName;
     }
+
+
 
     public void setRecipeName(String recipeName) {
         this.recipeName = recipeName;
@@ -51,4 +70,14 @@ public class NewRecipeDTO {
     public void setUnitIds(ArrayList<Long> unitIds) {
         this.unitIds = unitIds;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
 }
