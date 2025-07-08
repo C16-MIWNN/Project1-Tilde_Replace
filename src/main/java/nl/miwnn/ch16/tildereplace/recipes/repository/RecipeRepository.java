72c8dba992c58a720c67a65b885c9540a2ef91d5
Remi <heyremi@gmail.com>
2025-07-08 12:52:29 +0200

    Add method find recipe by tag

--- src/main/java/nl/miwnn/ch16/tildereplace/recipes/repository/RecipeRepository.java
+++ src/main/java/nl/miwnn/ch16/tildereplace/recipes/repository/RecipeRepository.java
@@ -2,12 +2,17 @@ package nl.miwnn.ch16.tildereplace.recipes.repository;
 
 import nl.miwnn.ch16.tildereplace.recipes.model.Recipe;
 
+import nl.miwnn.ch16.tildereplace.recipes.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     Optional<Recipe> findRecipeByRecipeName(String name);
+
+    List<Recipe> findRecipesByTags(List<Tag> tags);
+
 }
