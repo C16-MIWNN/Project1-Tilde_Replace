c11e08c8ef161c3a71fd44ec929a63f720884786
Dennis Mei <dmei22@hotmail.com>
2025-07-08 12:11:47 +0200

    Added IngredientTest

--- src/test/java/nl/miwnn/ch16/tildereplace/recipes/IngredientTests.java
+++ src/test/java/nl/miwnn/ch16/tildereplace/recipes/IngredientTests.java
@@ -6,24 +6,54 @@ import nl.miwnn.ch16.tildereplace.recipes.model.Ingredient;
 import nl.miwnn.ch16.tildereplace.recipes.model.Unit;
 import org.hibernate.jdbc.Expectation;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class IngredientTests {
 
-    @Test
-    @DisplayName("Setting negative ingredient amount throws IllegalArgumentException")
-    void setNegativeIngredientAmountThrowsException() {
+    @Nested
+    @DisplayName("When Ingredient exists")
+    class whenIngredientExists {
+
         // Arrange
-        Ingredient ingredient = new Ingredient();
+        private final Ingredient ingredient = new Ingredient();
 
-        // Act
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> ingredient.setAmount(-5));
+        @Nested
+        @DisplayName("Setting amount to negative value")
+        class settingAmountToNegativeValue {
 
-        // Assert
-        String expectedMessage = "Ingredient amount must be 0 or above";
-        String actualMessage = exception.getMessage();
-        assertEquals(expectedMessage, actualMessage);
+            @ParameterizedTest(name = " amount: {0}")
+            @ValueSource(ints = {-1, -5, -100})
+            @DisplayName("Throws IllegalArgumentException")
+            void throwsIllegalArgumentException(int negativeValue) {
+
+                // Act, Assert
+                assertThrows(IllegalArgumentException.class,
+                        () -> ingredient.setAmount(negativeValue));
+
+            }
+
+            @ParameterizedTest(name = " amount: {0}")
+            @ValueSource(ints = {-1, -5, -100})
+            @DisplayName("Throws IllegalArgumentException message")
+            void setAmountTo(int negativeValue) {
+
+                // Arrange
+                Exception exception = assertThrows(IllegalArgumentException.class,
+                        () -> ingredient.setAmount(negativeValue));
+
+                String expectedMessage = "Ingredient amount must be 0 or above";
+                assertEquals(expectedMessage, exception.getMessage());
+            }
+
+        }
+
     }
 
 }
